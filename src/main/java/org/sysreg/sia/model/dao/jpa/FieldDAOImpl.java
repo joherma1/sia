5cf21c4f45e2cdce9cc042a8cccb263bf95024db
Jose Antonio Hernández Martínez <joherma1@gmail.com>
2014-07-03 22:45:26 +0200

    Log4j 2 basic configuration

--- src/main/java/org/sysreg/sia/model/dao/jpa/FieldDAOImpl.java
+++ src/main/java/org/sysreg/sia/model/dao/jpa/FieldDAOImpl.java
@@ -1,21 +1,22 @@
 package org.sysreg.sia.model.dao.jpa;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import org.sysreg.sia.model.Field;
-import org.sysreg.sia.model.Parcel;
 import org.sysreg.sia.model.User;
 import org.sysreg.sia.model.dao.FieldDAO;
 
-import java.util.ArrayList;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import java.util.List;
 
 @Repository
 public class FieldDAOImpl implements FieldDAO {
+
+    static final Logger logger = LogManager.getLogger(FieldDAO.class.getName());
 
 	@PersistenceContext
 	private EntityManager entityManager;
@@ -25,7 +26,9 @@ public class FieldDAOImpl implements FieldDAO {
     public List<Field> findByUser(User user) {
         Query q = entityManager.createQuery("from Field where user = ?1", Field.class);
         q.setParameter(1, user);
+        logger.debug(q.unwrap(org.hibernate.Query.class).getQueryString());
         List<Field> results = q.getResultList();
+        logger.debug("Fields count: " + results.size());
         return results;
     }
 
